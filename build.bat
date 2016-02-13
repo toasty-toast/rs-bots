e4c07fc8477941f70053978ff736d73542807d3a
Brian McDonald <bmcdonald916@gmail.com>
2016-02-13 15:32:45 -0500

    Add build batch script
    Running this batch script will build all bots in the repository
    The jar files needed by OSBot will appear in the builds directory

---
+++ build.bat
@@ -0,0 +1,10 @@
+if exist .\build rmdir /Q /S .\build
+
+mkdir .\build
+
+javac -d .\build  -cp "dependencies/OSBot 2.4.39.jar;" catherby_fisher\src\CatherbyFisher.java
+jar cfe build\CatherbyFisher.jar CatherbyFisher -C .\build CatherbyFisher.class -C .\build CatherbyFisher$State.class -C .\build CatherbyFisher$1.class
+
+del .\build\*.class
+
+pause
