859f6e8feea51f74238ee6657d595b8ba0951e8f
Brian McDonald <bmcdonald916@gmail.com>
2016-02-14 11:46:57 -0500

    Remove pause statement from build.bat
    Cmd window now closes when build completes

--- build.bat
+++ build.bat
@@ -6,5 +6,3 @@ javac -d .\build  -cp "dependencies/OSBot 2.4.39.jar;" catherby_fisher\src\Cathe
 jar cfe build\CatherbyFisher.jar CatherbyFisher -C .\build CatherbyFisher.class -C .\build CatherbyFisher$State.class -C .\build CatherbyFisher$1.class
 
 del .\build\*.class
-
-pause
