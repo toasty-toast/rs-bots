dab653435fad37de3a8bc745150004e021e5934a
Brian McDonald <bmcdonald916@gmail.com>
2016-02-14 15:56:36 -0500

    Add herb cleaning bot to build script

--- build.bat
+++ build.bat
@@ -5,4 +5,7 @@ mkdir .\build
 javac -d .\build  -cp "dependencies/OSBot 2.4.39.jar;" catherby_fisher\src\CatherbyFisher.java
 jar cfe build\CatherbyFisher.jar CatherbyFisher -C .\build CatherbyFisher.class -C .\build CatherbyFisher$State.class -C .\build CatherbyFisher$1.class
 
+javac -d .\build -cp "dependencies/OSBot 2.4.39.jar;" herb_cleaner\src\HerbCleaner.java
+jar cfe build\HerbCleaner.jar HerbCleaner -C .\build HerbCleaner.class -C .\build HerbCleaner$State.class -C .\build HerbCleaner$1.class
+
 del .\build\*.class
