f5229378063e7ce3dba455e3e0328e49e3789ca3
Brian McDonald <bmcdonald916@gmail.com>
2016-03-05 13:37:00 -0500

    Fix fisher path in build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ if exist .\build rmdir /Q /S .\build
 
 mkdir .\build
 
-javac -d .\build  -cp "dependencies/*;" catherby_fisher\Fisher.java catherby_fisher\FisherUI.java shared\XPReporter.java shared\SkillUtils.java
+javac -d .\build  -cp "dependencies/*;" fisher\Fisher.java fisher\FisherUI.java shared\XPReporter.java shared\SkillUtils.java
 jar cfe build\Fisher.jar Fisher -C .\build Fisher.class -C .\build Fisher$State.class -C .\build Fisher$Mode.class -C .\build Fisher$1.class -C .\build Fisher$2.class -C .\build FisherUI.class -C .\build FisherUI$buttonListener.class -C .\build FisherUI$1.class -C .\build SkillUtils.class -C .\build SkillUtils$1.class -C .\build SkillUtils$2.class -C .\build XPReporter.class
 
 javac -d .\build -cp "dependencies/*;" herb_cleaner\HerbCleaner.java shared\XPReporter.java shared\SkillUtils.java
