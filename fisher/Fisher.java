f9d0292ad392e22d57b764b575972bb75eee4919
Brian McDonald <bmcdonald916@gmail.com>
2016-03-07 16:22:49 -0500

    Comment fishing bot

--- fisher/Fisher.java
+++ fisher/Fisher.java
@@ -17,58 +17,109 @@ import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 import org.osbot.rs07.utility.ConditionalSleep;
 
+/**
+ * This class contains a script for use in OSBot that will fish
+ * in multiple possible locations and track experience gained and fish caught.
+ * 
+ * @author Brian McDonald
+ */
 @ScriptManifest(author="ToastyToast", info = "", logo = "", name = "Fisher", version = 0.1)
 public class Fisher extends Script {
+	// fishing spot IDs
 	private static final int CAGE_HARPOON_FISH_SPOT = 1519;
 	private static final int NET_BAIT_FISH_SPOT = 1525;
 	
+	// individual areas to go to before looking for a fishing spot
 	private static final Area DRAYNOR_FISHING_AREA_1 = new Area(3084, 3231, 3088, 3226);
 	private static final Area CATHERBY_FISHING_AREA_1 = new Area(2835, 3434, 2841, 3432);
 	private static final Area CATHERBY_FISHING_AREA_2 = new Area(2843, 3433, 2848, 3430);
 	private static final Area CATHERBY_FISHING_AREA_3 = new Area(2851, 3428, 2857, 3423);
 	private static final Area CATHERBY_FISHING_AREA_4 = new Area(2857, 3429, 2862, 3426);
 	
+	// all fishing locations for each area
 	private static final Area[] DRAYNOR_FISHING_AREA = {DRAYNOR_FISHING_AREA_1};
 	private static final Area[] CATHERBY_FISHING_AREA = {CATHERBY_FISHING_AREA_1, CATHERBY_FISHING_AREA_2, CATHERBY_FISHING_AREA_3, CATHERBY_FISHING_AREA_4};
 	
+	// banks
 	private static final Area DRAYNOR_BANK = Banks.DRAYNOR;
 	private static final Area CATHERBY_BANK = Banks.CATHERBY;
 	
+	// count fish caught
 	private int shrimpCount;
 	private int anchovieCount;
 	private int tunaCount;
 	private int swordfishCount;
 	
-	private boolean singleFishingArea;
-	private int fishSpotId;
-	private String fishingTool;
-	private String fishSpotInteractAction;
-	private Area currentBank;
-	private Area[] legalFishingAreas;
+	private boolean singleFishingArea;		// true if multiple areas for current location
+	private int fishSpotId;					// id for current type of fishing spot
+	private String fishingTool;				// what is needed to fish (like a harpoon)
+	private String fishSpotInteractAction;	// action to use on fishing spot to begin fishing
+	private Area currentBank;				// bank to use for current location
+	private Area[] legalFishingAreas;		// areas to be in before looking for fishing spots
 	
-	private Object uiLock;
+	private Object uiLock;	// block script until fishing mode is chosen in UI
 	
-	private State state;
-	private Mode mode;
+	private State state;	// track state of bot
+	private Mode mode;		// type of fishing to do - chosen in pop-up UI
 	
+	/**
+	 * Modes of fishing that specify location and fish to catch
+	 */
 	public enum Mode {
+		/**
+		 * Catch shrimp and anchovies with a small net at Draynor and bank them
+		 */
 		DRAYNOR_SHRIMP_ANCHOVIES,
+		
+		/**
+		 * Catch tuna and swordfish with a harpoon at Catherby and bank them 
+		 */
 		CATHERBY_TUNA_SWORDFISH
 	}
 	
+	/**
+	 * States used to control logical flow of bot
+	 */
 	private enum State {
+		/**
+		 * Initialize inventory and prepare for fishing
+		 */
 		INIT,
+		
+		/**
+		 * Go to bank and deposit all fish
+		 */
 		BANK,
+		
+		/**
+		 * Go to fishing area and begin fishing
+		 */
 		FISH,
+		
+		/**
+		 * Sleep process until player is idle
+		 */
 		WAIT
 	}
 	
+	/**
+	 * Perform setup before bot begins running
+	 * 
+	 * Show a UI allowing the user to choose the location to fish at 
+	 * and perform general setup for that location.
+	 * 
+	 * This method is automatically called by the OSBot client.  Do not call explicitly.
+	 */
 	@Override
 	public void onStart() {
+		// create a new UI to choose the mode of fishing
 		uiLock = new Object();
-		mode = null;
 		FisherUI ui = new FisherUI(this, uiLock);
 		
+		// initialize mode to null so we know if the UI sets it or not
+		mode = null;
+		
+		// block current thread until UI chooses mode
 		synchronized(uiLock) {
 			try {
 				uiLock.wait();
@@ -77,22 +128,34 @@ public class Fisher extends Script {
 			}
 		}
 		
+		// UI closed without choosing a mode, so just exit
 		if(mode == null) {
 			stop(false);
 		}
 		
+		// get rid of the UI since we don't need it anymore
 		ui.dispose();
 		
+		
+		// initialize fish caught counters
 		shrimpCount = 0;
 		anchovieCount = 0;
 		tunaCount = 0;
 		swordfishCount = 0;
 		
-		state = State.INIT;
-		
+		// start tracking fishing XP gained
 		experienceTracker.start(Skill.FISHING);
+
+		state = State.INIT;
 	}
 	
+	/**
+	 * Main logic loop for bot
+	 * 
+	 * Perform tasks based on current state of bot
+	 * 
+	 * This method is automatically called by the OSBot client.  Do not call explicitly.
+	 */
 	@Override
 	public int onLoop() throws InterruptedException {
 		switch(state) {
@@ -107,6 +170,7 @@ public class Fisher extends Script {
 				break;
 			case WAIT:
 				waitUntilIdle();
+				// bank if we have full inventory of fish
 				if(inventory.isFull()) {
 					state = State.BANK;
 				} else {
@@ -118,8 +182,17 @@ public class Fisher extends Script {
 		return random(500, 2000);
 	}
 	
+	/**
+	 * Perform cleanup and exit operations on bot exit
+	 * 
+	 * Report XP gained to overall tracking server and print other relevant
+	 * information about the bot run to the logger.
+	 * 
+	 * This method is automatically called by the OSBot client.  Do not call explicitly.
+	 */
 	@Override
 	public void onExit() {
+		// report XP gained to server
 		try {
 			XPReporter.reportXP(Skill.FISHING, experienceTracker.getGainedXP(Skill.FISHING));
 		} catch (IOException e) {}
@@ -140,6 +213,14 @@ public class Fisher extends Script {
 		log("Fishing XP/hr: " + experienceTracker.getGainedXPPerHour(Skill.FISHING));
 	}
 	
+	/**
+	 * Process messages in the Runescape message dialog
+	 * 
+	 * Watch for messages about catching fish and use them to count the total
+	 * number of fish caught.
+	 * 
+	 * This method is automatically called by the OSBot client.  Do not call explicitly.
+	 */
 	@Override
 	public void onMessage(Message m) {
 		switch(m.getMessage()) {
@@ -158,28 +239,34 @@ public class Fisher extends Script {
 		}
 	}
 	
+	/**
+	 * Draw information about the bot's status in a banner on the top of the screen.
+	 * 
+	 * This method is automatically called by the OSBot client.  Do not call explicitly.
+	 */
 	@Override
 	public void onPaint(Graphics2D g) {
+		// don't start drawing the banner until a mode has been chosen
 		if(mode == null) {
 			return;
 		}
 		
-		int xpGained;
-		int xpPerHour;
-		int levelsGained;
-		
-		xpGained = experienceTracker.getGainedXP(Skill.FISHING);
-		xpPerHour = experienceTracker.getGainedXPPerHour(Skill.FISHING);
-		levelsGained = experienceTracker.getGainedLevels(Skill.FISHING);
+		// get status of skill level and XP
+		int xpGained = experienceTracker.getGainedXP(Skill.FISHING);
+		int xpPerHour = experienceTracker.getGainedXPPerHour(Skill.FISHING);
+		int levelsGained = experienceTracker.getGainedLevels(Skill.FISHING);
 		
 		super.onPaint(g);
 		
+		// draw background of banner
 		g.setColor(new Color(0x093145));
 		g.fillRect(0, 0, 515, 50);
 		
+		// set color of words on banner
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial", Font.BOLD, 14));
 		
+		// draw fish caught
 		switch(mode) {
 			case DRAYNOR_SHRIMP_ANCHOVIES:
 				g.drawString("Shrimp Caught: " + formatXpVal(shrimpCount), 5, 20);
@@ -191,12 +278,21 @@ public class Fisher extends Script {
 				break;
 		}
 		
+		// draw generic info about levels and XP
 		g.drawString("Current Fishing Level: " + skills.getStatic(Skill.FISHING), 180, 20);
 		g.drawString("Levels Gained: " + levelsGained, 180, 40);
 		g.drawString("XP Gained: " + formatXpVal(xpGained), 380, 20);
 		g.drawString("XP/hr: " + formatXpVal(xpPerHour), 380, 40);
 	}
 	
+	/**
+	 * Set mode of fishing for bot
+	 * 
+	 * Set the current location and type of fishing to do.  This method is
+	 * intended to be called only before the bot actually begins running.
+	 * 
+	 * @param mode Mode indicating location and type of fishing to do
+	 */
 	public void setMode(Mode mode) {
 		this.mode = mode;
 		
@@ -220,6 +316,14 @@ public class Fisher extends Script {
 		}
 	}
 	
+	/**
+	 * Go to bank and prepare inventory to start fishing
+	 * 
+	 * Move player to bank and deposit all items except what is
+	 * needed for fishing.  Once complete proceed to the FISH state.
+	 * 
+	 * @throws InterruptedException
+	 */
 	private void initInventory() throws InterruptedException {
 		walkToBank();
 		
@@ -228,16 +332,20 @@ public class Fisher extends Script {
 			sleep(random(500, 1000));
 		}
 		
+		// empty inventory
 		bank.depositAllExcept(fishingTool);
 		sleep(random(500, 1000));
 		
+		// check if tool already in inventory
 		if(!inventory.contains(fishingTool)) {
-			stop(false);
+			// check if player has a fishing tool in bank
 			if(bank.contains(fishingTool)) {
 				bank.withdraw(fishingTool, 1);
 				sleep(random(500, 1000));
 			} else {
+				// notify player that they don't have the items to fish and exit
 				JOptionPane.showMessageDialog(null, "No " + fishingTool + " found in inventory or bank");
+				stop(false);
 			}
 		}
 		
@@ -246,6 +354,12 @@ public class Fisher extends Script {
 		state = State.FISH;
 	}
 	
+	/**
+	 * Walk to bank
+	 * 
+	 * Walk player to bank and only return once player is successfully standing
+	 * in the bank
+	 */
 	@SuppressWarnings("deprecation")
 	private void walkToBank() {
 		while(!currentBank.contains(myPlayer())) {
@@ -258,6 +372,13 @@ public class Fisher extends Script {
 		}
 	}
 	
+	/**
+	 * Go to bank and deposit all fish
+	 * 
+	 * Everything in the inventory except the fishing tool will be deposited.
+	 * 
+	 * @throws InterruptedException
+	 */
 	private void bank() throws InterruptedException {
 		walkToBank();
 		
@@ -266,6 +387,7 @@ public class Fisher extends Script {
 			sleep(random(500, 1000));
 		}
 		
+		// deposit all fish
 		bank.depositAllExcept(fishingTool);
 		sleep(random(500, 1000));
 		bank.close();
@@ -273,24 +395,43 @@ public class Fisher extends Script {
 		state = State.FISH;
 	}
 	
+	/**
+	 * Go to fishing area and begin fishing
+	 * 
+	 * Walk to the fishing area and look for fishing spots.  Once fishing
+	 * proceed to IDLE state.
+	 * 
+	 * @throws InterruptedException
+	 */
 	private void fish() throws InterruptedException {
+		// check if we're in a fishing area
 		if(inFishingArea()) {
 			NPC fishingSpot = npcs.closest(fishSpotId);
+			
+			// fish is a fishing spot is available
 			if(fishingSpot != null) {
 				fishingSpot.interact(fishSpotInteractAction);
 				state = State.WAIT;
 			} else {
+				// check if there is another fishing area to go to
 				if(singleFishingArea) {
+					// no other fishing area so just wait for a spot to appear
 					waitForFishingSpot();
 				} else {
 					goToNextFishingArea();
 				}
 			}
 		} else {
+			// not in fishing area so go there
 			goToNextFishingArea();
 		}
 	}
 	
+	/**
+	 * Wait until the player is idle
+	 * 
+	 * Return only when the player is not moving or animating
+	 */
 	private void waitUntilIdle() {
 		new ConditionalSleep(Integer.MAX_VALUE, random(2000, 3000)) {
 			@Override
@@ -300,6 +441,11 @@ public class Fisher extends Script {
 		}.sleep();
 	}
 	
+	/**
+	 * Wait for a fishing spot to appear
+	 * 
+	 * Return only when a usable fishing spot has appeared
+	 */
 	private void waitForFishingSpot() {
 		new ConditionalSleep(Integer.MAX_VALUE, random(2000, 3000)) {
 			@Override
@@ -309,6 +455,11 @@ public class Fisher extends Script {
 		}.sleep();
 	}
 	
+	/**
+	 * Check if a player is in a legal fishing area
+	 * 
+	 * @return True if the player is standing in one of the legal fishing areas
+	 */
 	private boolean inFishingArea() {
 		for(Area area : legalFishingAreas) {
 			if(area.contains(myPlayer())) {
@@ -318,6 +469,12 @@ public class Fisher extends Script {
 		return false;
 	}
 	
+	/**
+	 * Walk to the next fishing area in the list of legal fishing areas
+	 * 
+	 * Move the player to the next fishing area and return only when the
+	 * player is successfully standing in that area
+	 */
 	@SuppressWarnings("deprecation")
 	private void goToNextFishingArea() {
 		Area next = getNextFishingArea();
@@ -328,12 +485,20 @@ public class Fisher extends Script {
 				LinkedList<Position> path = pathFinder.findPath(myPlayer().getPosition(), next.getRandomPosition());
 				if(path != null) {
 					localWalker.walkPath(path);
+					waitUntilIdle();
 				}
-				waitUntilIdle();
 			}
 		}
 	}
 	
+	/**
+	 * Get the next fishing area in the list of legal fishing areas
+	 * 
+	 * Find the next fishing area the player should go to.  If at the end
+	 * of the array, loop back to the first location in the array.
+	 * 
+	 * @return Next fishing area player should walk to
+	 */
 	private Area getNextFishingArea() {
 		Area next = legalFishingAreas[0];
 		
@@ -347,6 +512,16 @@ public class Fisher extends Script {
 		return next;
 	}
 	
+	
+	/**
+	 * Format a value to use Runescape "k" notation if large enough
+	 * 
+	 * If the value is greater than 1000, it will be divided by 1000 and a
+	 * "k" will be appended.  If not, the value will remain unchanged.
+	 * 
+	 * @param val Value to format
+	 * @return Input value formatted to Runescape's "k" notation
+	 */
 	private String formatXpVal(int val) {
 		String ret;
 		
--- fisher/FisherUI.java
+++ fisher/FisherUI.java
@@ -12,15 +12,26 @@ import org.osbot.rs07.script.Script;
 import javax.swing.BoxLayout;
 import java.awt.GridLayout;
 
-
+/**
+ * This class contains a UI that allows the user to choose
+ * what fishing the fishing bot will do
+ * 
+ * @author Brian McDonald
+ */
 @SuppressWarnings("serial")
 public class FisherUI extends JFrame {
-	private Fisher fisher;
-	private Object fisherLock;
+	private Fisher fisher;			// instance of the bot we are controlling
+	private Object fisherLock;		// synchronization lock provided by bot
 	
-	private JButton btnCatherby;
-	private JButton btnDraynor;
+	private JButton btnCatherby;	// button for Catherby fishing
+	private JButton btnDraynor;		// button for Draynor fishing
 	
+	/**
+	 * Create a new UI for choosing what fishing the bot will do
+	 * 
+	 * @param parent Instance of the bot we are controlling
+	 * @param lock Synchronization lock provided by the bot
+	 */
 	public FisherUI(Fisher parent, final Object lock) {
 		super();
 		
@@ -33,6 +44,8 @@ public class FisherUI extends JFrame {
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		// release block on bot thread when window is closed
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -40,10 +53,12 @@ public class FisherUI extends JFrame {
 			}
 		});
 		
+		// main JPanel to hold all buttons
 		JPanel pnlLocation = new JPanel();
 		pnlLocation.setLayout(new GridLayout(2, 1, 0, 8));
 		getContentPane().add(pnlLocation);
 		
+		// single button listener instance to apply to all buttons
 		buttonListener listener = new buttonListener();
 
 		btnDraynor = new JButton("Draynor - Shrimp & Anchovies");
@@ -58,6 +73,12 @@ public class FisherUI extends JFrame {
 		setVisible(true);
 	}
 	
+	/**
+	 * Set bot mode when button is clicked
+	 * 
+	 * Set mode in bot and release block on bot to allow the bot 
+	 * to continue execution
+	 */
 	private class buttonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource().equals(btnCatherby)) {
@@ -69,6 +90,9 @@ public class FisherUI extends JFrame {
 		}
 	}
 	
+	/**
+	 * Notify the bot that it can proceed with execution
+	 */
 	private void unlockFisher() {
 		synchronized(fisherLock) {
 			fisherLock.notifyAll();
