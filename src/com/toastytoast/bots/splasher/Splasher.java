660cb9cb124569b06c79dfeab8112298babc5c54
Brian McDonald <bmcdonald916@gmail.com>
2016-03-08 17:26:24 -0500

    Add splasher bot

--- src/com/toastytoast/bots/splasher/Splasher.java
+++ src/com/toastytoast/bots/splasher/Splasher.java
@@ -1,6 +1,10 @@
 package com.toastytoast.bots.splasher;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import org.osbot.rs07.api.model.NPC;
 import org.osbot.rs07.api.ui.Skill;
@@ -67,13 +71,21 @@ public class Splasher extends Script {
 	public int onLoop() throws InterruptedException {
 		switch(state) {
 			case INIT:
+				if(readyToSplash()) {
+					state = State.ATTACK;
+				} else {
+					JOptionPane.showMessageDialog(null, "Your magic attack bonus is not low enough to splash");
+					stop(false);
+				}
 				break;
 			case ATTACK:
 				attack();
 				break;
 			case INTERACT:
+				moveMouse();
 				break;
 			case WAIT:
+				waitUntilInteract();
 				break;
 		}
 		return random(250, 500);
@@ -98,7 +110,70 @@ public class Splasher extends Script {
 		log("Magic XP/hr: " + experienceTracker.getGainedXPPerHour(Skill.MAGIC));
 	}
 	
+	private boolean readyToSplash() {
+		return true;
+	}
+	
+	/**
+	 * Attack a spider
+	 * 
+	 * Attack a random spider that is not fighting someone else
+	 */
+	@SuppressWarnings("deprecation")
 	private void attack() {
+		List<NPC> allNPCs = npcs.getAll();
+		ArrayList<NPC> spiders = new ArrayList<NPC>();
+		ArrayList<NPC> availableSpiders = new ArrayList<NPC>();
+		NPC spiderToAttack = null;
 		
+		// get all spiders in area
+		for(NPC npc : allNPCs) {
+			if("Spider".equals(npc.getName())) {
+				spiders.add(npc);
+			}
+		}
+		
+		// get spiders we can fight
+		for(NPC spider : spiders) {
+			if(spider.exists() && spider.getHealth() > 0 && !spider.isUnderAttack()) {
+				availableSpiders.add(spider);
+			}
+		}
+		
+		if(availableSpiders.size() < 1) {
+			// notify user that there are no spiders to attack and exit
+			JOptionPane.showMessageDialog(null, "No available spiders here");
+			stop(false);
+		}
+		
+		// just attack the first spider in the list
+		spiderToAttack = availableSpiders.get(0);
+		if(spiderToAttack.interact("Attack")) {
+			state = State.WAIT;
+		}
+	}
+	
+	/**
+	 * Move the mouse to a random location on the screen then off the screen
+	 */
+	private void moveMouse() {
+		mouse.move(random(0, 500), random(0, 500));
+		
+		try {
+			sleep(random(250, 500));
+		} catch (InterruptedException e) {}
+		
+		mouse.moveOutsideScreen();
+	}
+	
+	/**
+	 * Sleep for 3.5 to 4.5 minutes (less than the logout timer)
+	 */
+	private void waitUntilInteract() {
+		try {
+			sleep(random(210000, 270000));
+		} catch (InterruptedException e) {}
+		
+		state = State.INTERACT;
 	}
 }
